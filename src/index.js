f13aef1886038f14e29b3cc57c900a3f33a807e7
Samuel Alves <samueltheodoro21@gmail.com>
2022-12-18 20:11:41 -0300

    making an inicial data fetching and setting the data as a global state

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { App } from './components/App';
 import './index.css';
-import App from './components/App';
+import { CurrentWeatherContext } from './contexts/CurrentWeatherContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <CurrentWeatherContext>
     <App />
-  </React.StrictMode>
+  </CurrentWeatherContext>
 );
