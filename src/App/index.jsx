f7454d58c8cb7179f4767d3a445badc2e331d664
Samuel Alves <samueltheodoro21@gmail.com>
2023-01-07 21:13:51 -0300

    deployment

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { HashRouter, Route, Routes } from "react-router-dom"
 import { Home } from "../Pages/Home"
 import { CitySearchPage } from "../Pages/CitySearchPage"
 import { Error } from "../Pages/Error"
@@ -6,12 +6,12 @@ import { Error } from "../Pages/Error"
 
 export const App = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes basename="/weather-app">
-        <Route path="/" element={<Home />}/>
-        <Route path="/city/:cityName" element={<CitySearchPage />} />
-        <Route path="/error" element={<Error />} />
+    <HashRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/city/:cityName" element={<CitySearchPage />} />
+          <Route path="/error" element={<Error />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
